5addee8e3cd255b584c4aa763fc66459f9471fa4
REYNES Lucas <lucasreynes@hotmail.fr>
2019-04-03 11:32:14 +0200

    Initial commit :rocket:

---
+++ src/scala/Main.scala
@@ -0,0 +1,5 @@
+object Main extends App {
+
+  val url = "https://calendar.google.com/calendar/ical/8hi4kdfj3fg66nflut86f3rncc%40group.calendar.google.com/private-d1e99dcebf3441a1139ca70e907d6160/basic.ics"
+
+}
